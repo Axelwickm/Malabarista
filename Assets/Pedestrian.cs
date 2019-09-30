97bf86d64edfc368b555eff764307964a151e69f
Axel Wickman <axelwickm@gmail.com>
2019-09-30 18:55:31 -0300

    Started working on pedestrian system

---
+++ Assets/Pedestrian.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestrian : MonoBehaviour
+{
+    public Spawner spawner;
+    private GameObject goalWaypoint;
+    private NavMeshAgent nmAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nmAgent = GetComponent<NavMeshAgent>();
+        // Choose where to go
+        int wpIndex = Random.Range(0, spawner.gameObject.transform.childCount-1);
+        goalWaypoint = spawner.gameObject.transform.GetChild(wpIndex).gameObject;
+        nmAgent.destination = goalWaypoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public int MaxPedestrians = 10;
+    public double spawnRate = 0.05; // Hz
+    public GameObject Pedestrian;
+    private int currentPedestrians = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.value < spawnRate * Time.deltaTime && currentPedestrians < MaxPedestrians)
+        {
+            // Spawn a new agent at waypoint
+            int wpIndex = Random.Range(0, gameObject.transform.childCount - 1);
+            Vector3 position = gameObject.transform.GetChild(wpIndex).transform.position;
+
+            Instantiate(Pedestrian, position, Quaternion.identity);
+            print("Made pedestrian!");
+            currentPedestrians++;
+        }
+    }
+
+    // Pedestrian just done with life
+    void DoneWithLife()
+    {
+        currentPedestrians--;
+        print(":( it died");
+    }
+}
